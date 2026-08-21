1503ab9218a9ccaea2ef29393821a484cb59ea07
Editzx7777772hshhs <igknight778@gmail.com>
2026-08-21 21:32:55 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,12 @@
-window.AURA_FIREBASE_CONFIG = {
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: "AIzaSyCjhJlJ4PAsYsGcjrEfKYz4P4SvUDJV140",
   authDomain: "calestial-sound.firebaseapp.com",
   projectId: "calestial-sound",
@@ -7,3 +15,7 @@ window.AURA_FIREBASE_CONFIG = {
   appId: "1:783637882927:web:b90ae15226d48c40193eda",
   measurementId: "G-6WZYX6JTSB"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
